468cb10a3b4dbdde3d67c6173022d12f5337ab10
Vince Chan <vince.yp.chan@gmail.com>
2017-04-23 20:17:44 -0700

    run simulator
    - Format output to conform to what simulator expect
    - Format code

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -39,8 +39,8 @@ void KalmanFilter::Update(const VectorXd &z) {
   MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
   MatrixXd K = PHt * Si;
-  
-  //new estimate
+
+  // new estimate
   x_ = x_ + (K * y);
   long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
@@ -51,20 +51,20 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * update the state by using Extended Kalman Filter equations
    */
-  
+
   // Convert prediction from Cartesian to Polar
   float px = x_(0);
   float py = x_(1);
   float vx = x_(2);
   float vy = x_(3);
-  float ro = sqrt(px*px + py*py);
+  float ro = sqrt(px * px + py * py);
   float phi = atan2(py, px);
-  float ro_dot = (px*vx + py*vy) / ro;
+  float ro_dot = (px * vx + py * vy) / ro;
   VectorXd z_pred(3);
   z_pred << ro, phi, ro_dot;
-  
+
   VectorXd y = z - z_pred;
-  
+
   // normalize phi to be between -PI and PI
   while (y(1) > PI) {
     y(1) -= 2 * PI;
@@ -73,14 +73,14 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     y(1) += 2 * PI;
   }
   assert(y(1) >= -PI && y(1) <= PI);
-  
+
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
   MatrixXd K = PHt * Si;
-  
-  //new estimate
+
+  // new estimate
   x_ = x_ + (K * y);
   long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "tools.h"
+#include <iostream>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -23,48 +23,48 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   //  * the estimation
   assert(estimations.size() > 0 && estimations.size() == ground_truth.size());
 
-  //accumulate squared residuals
-  for(int i=0; i < estimations.size(); ++i){
+  // accumulate squared residuals
+  for (int i = 0; i < estimations.size(); ++i) {
     VectorXd residual = estimations[i] - ground_truth[i];
     residual = residual.array() * residual.array();
     rmse += residual;
   }
 
-  //calculate the mean
+  // calculate the mean
   rmse = rmse / estimations.size();
 
-  //calculate the squared root
+  // calculate the squared root
   rmse = rmse.array().sqrt();
 
-  return rmse;  
+  return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
   /**
   TODO:
     * Calculate a Jacobian here.
   */
-  
-  MatrixXd Hj(3,4);
-  
-  //recover state parameters
+
+  MatrixXd Hj(3, 4);
+
+  // recover state parameters
   float px = x_state(0);
   float py = x_state(1);
   float vx = x_state(2);
   float vy = x_state(3);
 
-  //pre-compute a set of terms to avoid repeated calculation
-  float c1 = px*px+py*py;
+  // pre-compute a set of terms to avoid repeated calculation
+  float c1 = px * px + py * py;
   float c2 = sqrt(c1);
-  float c3 = (c1*c2);
+  float c3 = (c1 * c2);
 
-  //check division by zero
+  // check division by zero
   assert(c1 >= 0.0001);
 
-  //compute the Jacobian matrix
-  Hj <<  (px/c2),              (py/c2),               0,     0,
-        -(py/c1),              (px/c1),               0,     0,
-        py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+  // compute the Jacobian matrix
+  Hj << (px / c2), (py / c2), 0, 0, 
+        -(py / c1), (px / c1), 0, 0,
+        py * (vx * py - vy * px) / c3, px * (px * vy - py * vx) / c3, px / c2, py / c2;
 
   return Hj;
 }
